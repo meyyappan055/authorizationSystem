cc9516b3a9e4acd7ac2bfcc94705d8e174bc5361
meyyappan055 <119162363+meyyappan055@users.noreply.github.com>
2024-04-06 17:05:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.static(path.join(__dirname, "views")));
 app.use("/auth", authRoutes); // Corrected
 
 app.listen(5000, function(){
-    console.log("Server running on port 3000");
+    console.log("Server running on port 5000");
 });
